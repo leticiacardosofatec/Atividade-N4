1acc4068f865fad819c3143a8ab070fb84b70b9f
leticiacardosofatec <168546895+leticiacardosofatec@users.noreply.github.com>
2024-06-05 09:06:46 -0300

    Update routes.tsx

--- vite-project/src/routes.tsx
+++ vite-project/src/routes.tsx
@@ -2,12 +2,14 @@ import Home from './pages/home/index.tsx'
 import Form from './pages/form/index'
 import Sobre from './pages/sobre/index.tsx'
 import NotFound from './pages/not-found/index.tsx'
+import Tarefas from './pages/tarefas/index'
 import { createBrowserRouter } from 'react-router-dom'
 
 const router = createBrowserRouter([
   { path: '/', element: <Home />, errorElement: <NotFound />},
   { path: '/form', element: <Form />},
-  { path: '/sobre', element: <Sobre />}
+  { path: '/sobre', element: <Sobre />},
+  { path: '/tarefas', element: <Tarefas />}
 ])
 
 export default router
