7b85e6a61f71d8aed8dacc729c45638aff701a7c
leticiacardosofatec <168546895+leticiacardosofatec@users.noreply.github.com>
2024-06-06 08:19:27 -0300

    Update index.tsx

--- vite-project/src/pages/tarefas/index.tsx
+++ vite-project/src/pages/tarefas/index.tsx
@@ -6,9 +6,12 @@ export default function Tarefas() {
         <>
             <h1>Tarefas Feitas nesse Projeto</h1>
             <ul>
-                <li>Reorganizar o projeto para garantir que fique mais fácil realizar a manutenção das páginas e dos componentes do projeto.</li>
-                <li>Levar toda a estrutura da página inicial para dentro de um arquivo que irá passar a ser o componente responsável por essa página.</li>  
-                <li>A página Sobre vai trazer informações sobre o nosso projeto e seu código deve ficar dentro do arquivo.</li>
+                <li>1-Reorganizar o projeto para garantir que fique mais fácil realizar a manutenção das páginas e dos componentes do projeto.</li>
+                <li>2-Levar toda a estrutura da página inicial para dentro de um arquivo que irá passar a ser o componente responsável por essa página.</li>  
+                <li>3-A página Home vai faze ruma breve apresentação do nosso projeto.</li>
+                <li>4-A página Sobre vai trazer informações sobre o nosso projeto e seu código deve ficar dentro do arquivo.</li>
+                <li>5-A página Form deverá ter os campos nome, e-mail, telefone e um botão de enviar.</li>
+                <li>6-A página Tarefas vai listar uma série de tarefas sobre o projeto.</li>
              </ul>
             <div><Link to="/">Voltar para Home</Link></div>
             <div><Link to="/form">Formulario</Link></div>
